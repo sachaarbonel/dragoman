c2ae6b6d20f415598b3c082f580a58e268896761
Sacha <sacha.arbonel@hotmail.fr>
2020-11-26 13:15:07 +0100

    refactor to transpile trait : generic+ phantomData

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 extern crate lazy_static;
 use rustpython_parser::parser;
 
+pub struct Python {}
+
 lazy_static! {
     static ref IDENTIFIER_MAP: std::collections::HashMap<String, &'static str> =
         vec![(r#"print"#.to_owned(), "println!")]
@@ -9,10 +11,11 @@ lazy_static! {
             .collect();
 }
 
-fn extract_type_string(value: &rustpython_parser::ast::StringGroup) -> ArgType {
+fn extract_type_string(value: &rustpython_parser::ast::StringGroup) -> ArgType<Python> {
     match value {
-        rustpython_parser::ast::StringGroup::Constant { ref value } => ArgType::String {
+        rustpython_parser::ast::StringGroup::Constant { ref value } => ArgType::<Python>::String {
             value: value.to_string(),
+            phantom: std::marker::PhantomData,
         },
         _ => unimplemented!(),
     }
@@ -20,29 +23,35 @@ fn extract_type_string(value: &rustpython_parser::ast::StringGroup) -> ArgType {
 
 // A certain type of expression.
 #[derive(PartialEq)]
-pub enum ExpressionType {
+pub enum ExpressionType<T> {
     FunctionCall {
         function_identifier: String,
-        function_args: Vec<ArgType>,
+        function_args: Vec<ArgType<T>>,
     },
 }
 
 #[derive(PartialEq)]
-pub enum ArgType {
-    String { value: String },
+pub enum ArgType<T> {
+    String {
+        value: String,
+        phantom: std::marker::PhantomData<T>,
+    },
 }
 
-impl std::fmt::Display for ArgType {
+impl std::fmt::Display for ArgType<Python> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match &*self {
-            ArgType::String { ref value } => write!(f, r#""{}""#, value),
+            ArgType::<Python>::String {
+                ref value,
+                phantom: _,
+            } => write!(f, r#""{}""#, value),
 
             _ => unimplemented!(),
         }
     }
 }
 
-impl std::fmt::Display for ExpressionType {
+impl std::fmt::Display for ExpressionType<Python> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match &*self {
             ExpressionType::FunctionCall {
@@ -67,7 +76,7 @@ impl std::fmt::Display for ExpressionType {
 fn extract_call(
     function: &rustpython_parser::ast::Expression,
     args: &Vec<rustpython_parser::ast::Expression>,
-) -> ExpressionType {
+) -> ExpressionType<Python> {
     let function = match function {
         rustpython_parser::ast::Located { location: _, node } => match node {
             rustpython_parser::ast::ExpressionType::Identifier { ref name } => name,
@@ -84,12 +93,17 @@ fn extract_call(
         },
         _ => unimplemented!(),
     };
-    ExpressionType::FunctionCall {
+    ExpressionType::<Python>::FunctionCall {
         function_identifier: function.to_owned(),
         function_args: vec![args],
     }
 }
 
+trait TranspilerTrait {
+    fn transpile(source: &str) -> String;
+}
+
+impl TranspilerTrait for Python {
 fn transpile(source: &str) -> String {
     let python_ast = parser::parse_expression(source).unwrap();
     let function_call = match python_ast {
@@ -105,10 +119,13 @@ fn transpile(source: &str) -> String {
     };
     function_call.to_string()
 }
+}
+
+
 
 fn main() {
     let python_source = r#"print("Hello world")"#;
-    let func_call = transpile(python_source);
+    let func_call = Python::transpile(python_source);
     println!("{:#?}", func_call);
 }
 
@@ -119,7 +136,7 @@ mod tests {
     #[test]
     fn ast_ser_hello_world() {
         let hello_world = r#"print("Hello world")"#;
-        let hello_world_rs = transpile(hello_world);
+        let hello_world_rs = Python::transpile(hello_world);
 
         assert_eq!(hello_world_rs, "println!(\"Hello world\")");
     }
